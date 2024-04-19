7ecc036cc6696bdfd9a941c043a1f44bb77e69c5
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-04-19 18:45:13 +0300

    create an example with bad independent async calls

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
+import asyncio
+import time
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+async def get_first_arg():
+    print("get_first_arg(): Start")
+    await asyncio.sleep(3)
+    print("get_first_arg(): End")
+    return 1
+
+
+async def get_second_arg():
+    print("get_second_arg(): Start")
+    await asyncio.sleep(3)
+    print("get_second_arg(): End")
+    return 2
+
+
 @app.get("/")
 async def index():
-    return {"message": "Hello, World!"}
+    start = time.perf_counter()
+    first_arg = await get_first_arg()
+    second_arg = await get_second_arg()
+    elapsed = time.perf_counter() - start
+    print(f"Execution time: {elapsed:0.2f}")
+    return {"first_arg": first_arg, "second_arg": second_arg}
