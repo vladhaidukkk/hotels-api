64412c480595c3506c70a96f3697c693e0ceb6ca
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-04-20 21:56:06 +0300

    create skeleton of get hotels endpoint

--- app/main.py
+++ app/main.py
@@ -1,29 +1,21 @@
-import asyncio
-import time
+from datetime import date
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
-async def get_first_arg():
-    print("get_first_arg(): Start")
-    await asyncio.sleep(3)
-    print("get_first_arg(): End")
-    return 1
-
-
-async def get_second_arg():
-    print("get_second_arg(): Start")
-    await asyncio.sleep(3)
-    print("get_second_arg(): End")
-    return 2
-
-
-@app.get("/")
-async def index():
-    start = time.perf_counter()
-    [first_arg, second_arg] = await asyncio.gather(get_first_arg(), get_second_arg())
-    elapsed = time.perf_counter() - start
-    print(f"Execution time: {elapsed:0.2f}")
-    return {"first_arg": first_arg, "second_arg": second_arg}
+@app.get("/hotels")
+def get_hotels(
+        location: str,
+        date_from: date,
+        date_to: date,
+        stars: Annotated[int | None, Query(ge=1, le=5)] = None,
+):
+    return {
+        "location": location,
+        "date_from": date_from,
+        "date_to": date_to,
+        "stars": stars,
+    }
