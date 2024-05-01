62f1ca515549d9fe8578ec8ca2ee60f60351b84a
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-05-01 22:36:42 +0300

    use dotenv to load env variables

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_USER = os.environ["DB_USER"]
+DB_PASS = os.environ["DB_PASS"]
+DB_NAME = os.environ["DB_NAME"]
