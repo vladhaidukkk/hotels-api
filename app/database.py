73e87fc5346929b5387e57a0bf80d10e59f1da9d
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-05-02 14:47:24 +0300

    replace sync sqlalchemy engine with async but use the same driver

--- app/database.py
+++ app/database.py
@@ -1,10 +1,20 @@
-from sqlalchemy import create_engine, text
+import asyncio
+
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from app.config import DB_URL
 
-engine = create_engine(url=DB_URL, echo=True)
+engine = create_async_engine(url=DB_URL, echo=True)
 
-with engine.connect() as conn:
-    res = conn.execute(text("SELECT VERSION()"))
-    version = res.first()[0]
-    print(version)
+
+async def print_version():
+    async with engine.connect() as conn:
+        res = await conn.execute(text("SELECT VERSION()"))
+        version_row = res.first()
+        if version_row:
+            version = version_row[0]
+            print(version)
+
+
+asyncio.run(print_version())
