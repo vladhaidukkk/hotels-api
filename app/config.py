f92afcfef325bdc12fe16e9f6c34e97ab5afe4bd
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-05-04 00:53:19 +0300

    improve db settings

--- app/config.py
+++ app/config.py
@@ -1,20 +1,25 @@
+from pydantic import Extra, Field, PostgresDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Extra
 
 
 class Settings(BaseSettings):
     db_host: str
-    db_port: int
+    db_port: int = Field(ge=1, le=65535)
     db_user: str
     db_pass: str
     db_name: str
 
-    @property
-    def db_url(self):
-        return (
-            f"postgresql+asyncpg://{self.db_user}:{self.db_pass}"
-            f"@{self.db_host}:{self.db_port}/{self.db_name}"
+    @computed_field
+    def db_url(self) -> str:
+        dsn = PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=self.db_user,
+            password=self.db_pass,
+            host=str(self.db_host),
+            port=self.db_port,
+            path=self.db_name,
         )
+        return dsn.unicode_string()
 
     model_config = SettingsConfigDict(env_file=".env", extra=Extra.ignore)
 
