aa2db4538b7ed1b24e63394a39330502a23bb922
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-05-06 23:05:12 +0300

    add more methods to base repo

--- app/db/base_repo.py
+++ app/db/base_repo.py
@@ -1,18 +1,34 @@
 from typing import Sequence, Type
 
-from sqlalchemy import select
+from sqlalchemy import ColumnExpressionArgument, select
 
 from app.db.core import Base, session_factory
 
 
 class BaseRepo[T: Base]:
     @classmethod
-    def model(cls) -> Type[T]:
+    def _model(cls) -> Type[T]:
         return cls.__orig_bases__[0].__args__[0]  # type: ignore
 
     @classmethod
-    async def get_all(cls) -> Sequence[T]:
+    async def get_by_id(cls, id_: int) -> T | None:
         async with session_factory() as session:
-            query = select(cls.model())
+            query = select(cls._model()).filter_by(id=id_)
+            result = await session.execute(query)
+            return result.scalars().one_or_none()
+
+    @classmethod
+    async def get_one_or_none(
+        cls, *filters: ColumnExpressionArgument[bool]
+    ) -> T | None:
+        async with session_factory() as session:
+            query = select(cls._model()).filter(*filters)
+            result = await session.execute(query)
+            return result.scalars().one_or_none()
+
+    @classmethod
+    async def get_all(cls, *filters: ColumnExpressionArgument[bool]) -> Sequence[T]:
+        async with session_factory() as session:
+            query = select(cls._model()).filter(*filters)
             result = await session.execute(query)
             return result.scalars().all()
