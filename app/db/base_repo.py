5cb8395db89f82787ca2a50d75a0b14d4e3e0f1b
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-05-06 20:29:08 +0300

    use new generics syntax

--- app/db/base_repo.py
+++ app/db/base_repo.py
@@ -1,13 +1,11 @@
-from typing import Generic, Sequence, Type, TypeVar
+from typing import Sequence, Type
 
 from sqlalchemy import select
 
 from app.db.core import Base, session_factory
 
-T = TypeVar("T", bound=Base)
 
-
-class BaseRepo(Generic[T]):
+class BaseRepo[T: Base]:
     @classmethod
     def model(cls) -> Type[T]:
         return cls.__orig_bases__[0].__args__[0]  # type: ignore
