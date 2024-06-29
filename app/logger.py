790c01b1f99178a769ba9bb5142bf83fcd5d3f5c
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-06-29 19:22:29 +0300

    paint logs with colors

--- app/logger.py
+++ app/logger.py
@@ -1,20 +1,23 @@
 import logging
 import time
 
+import colorlog
+
 from app.config import settings
 
 logger = logging.getLogger()
 
 
-class UTCFormatter(logging.Formatter):
+class UTCFormatter(colorlog.ColoredFormatter):
     converter = time.gmtime
 
 
 formatter = UTCFormatter(
-    "[%(asctime)s] [%(levelname)s] - %(message)s", datefmt="%Y-%m-%dT%H:%M:%S"
+    "[%(white)s%(asctime)s%(reset)s] [%(log_color)s%(levelname)s%(reset)s] - %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S",
 )
 
-stream_handler = logging.StreamHandler()
+stream_handler = colorlog.StreamHandler()
 stream_handler.setFormatter(formatter)
 
 logger.setLevel(logging.DEBUG if settings.env.debug else logging.INFO)
